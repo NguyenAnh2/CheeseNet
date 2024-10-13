b14c41aa812c3858e434b8607226cf5f9a6b98a3
NguyenAnh <anht9402@gmail.com>
2024-10-13 11:20:01 +0700

    change server

--- pages/login/index.js
+++ pages/login/index.js
@@ -15,7 +15,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

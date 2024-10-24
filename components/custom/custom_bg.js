2120b1abe3a784af1088d6e03a1e9a18760145f5
NguyenAnh <anht9402@gmail.com>
2024-10-24 16:40:13 +0700

    update all

--- components/custom/custom_bg.js
+++ components/custom/custom_bg.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "../../styles/Home.module.css";
 
 const images = [
   "/images/b1r.png",
@@ -50,7 +49,7 @@ const RandomImageBackground = () => {
   const positions = generateUniquePositions(repeatedImages.length); // Tạo danh sách vị trí không trùng
 
   return (
-    <div className={styles.background}>
+    <div className='background-custom-bg'>
       {repeatedImages.map((src, index) => {
         const randomRotation = Math.floor(Math.random() * 360);
         const position = positions[index]; // Lấy vị trí ngẫu nhiên nhưng không trùng
@@ -60,7 +59,7 @@ const RandomImageBackground = () => {
             key={index}
             src={src}
             alt={`image-${index}`}
-            className={styles.image}
+            className='image-custom-bg'
             style={{
               transform: `rotate(${randomRotation}deg)`,
               top: `${position.top}%`,
